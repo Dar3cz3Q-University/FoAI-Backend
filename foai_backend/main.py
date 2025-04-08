3e50a4c9c2834f1251caf1360115a42ff693fd10
Jakub Latawiec <latawiec.jakub@outlook.com>
2025-04-08 19:42:54 +0200

    fix: added CORS for localhost development

--- foai_backend/main.py
+++ foai_backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi_health import health
+from fastapi.middleware.cors import CORSMiddleware
 
 from foai_backend.routes.predict import predict
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def always_ok():
